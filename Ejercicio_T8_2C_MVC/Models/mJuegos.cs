0a3737b3e01242d59c868d12a70899720b0aad9e
EloyGPlexus <62422831+EloyGPlexus@users.noreply.github.com>
2020-04-14 16:04:52 +0200

    p4

---
+++ Ejercicio_T8_2C_MVC/Models/mJuegos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejercicio_T8_2C_MVC.Models
+{
+    public class mJuegos
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public string tipo { get; set; }
+    }
+}
