69273e0e1f588072952c465004ed8b11939ba193
EloyGPlexus <62422831+EloyGPlexus@users.noreply.github.com>
2020-04-14 16:10:42 +0200

    p5

---
+++ Ejercicio_T8_2C_MVC/Models/mApuestas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejercicio_T8_2C_MVC.Models
+{
+    public class mApuestas
+    {
+        public int ID { get; set; }
+        public int? IDJugador { get; set; }
+
+        public DateTime? fecha { get; set; }
+
+        public double? cantidad { get; set; }
+    }
+}
