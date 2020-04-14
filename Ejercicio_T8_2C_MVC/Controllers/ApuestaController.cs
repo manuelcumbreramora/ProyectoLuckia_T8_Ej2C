8cb28373046bd401bea91aad3bd9059fe42316ac
EloyGPlexus <62422831+EloyGPlexus@users.noreply.github.com>
2020-04-14 15:27:42 +0200

    p3

---
+++ Ejercicio_T8_2C_MVC/Controllers/ApuestaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio_T8_2C_MVC.Controllers
+{
+    public class ApuestaController : Controller
+    {
+        // GET: Apuesta
+        public ActionResult Apuesta()
+        {
+            return View();
+        }
+
+ 
+    }
+}
