28d99cc91ec70d4170657055d0a48855da78ea08
EloyGPlexus <62422831+EloyGPlexus@users.noreply.github.com>
2020-04-15 10:09:37 +0200

    Update mJuegos.cs

--- Ejercicio_T8_2C_MVC/Models/mJuegos.cs
+++ Ejercicio_T8_2C_MVC/Models/mJuegos.cs
@@ -7,6 +7,7 @@ namespace Ejercicio_T8_2C_MVC.Models
 {
     public class mJuegos
     {
+        //khj
         public int ID { get; set; }
         public string nombre { get; set; }
         public string tipo { get; set; }
