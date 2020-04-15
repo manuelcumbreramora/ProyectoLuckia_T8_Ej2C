39becdd9caa3427f1c2ed702f73dc017d251f88b
EloyGPlexus <62422831+EloyGPlexus@users.noreply.github.com>
2020-04-15 18:34:17 +0200

    Create AbstractDAO.cs

---
+++ Datos/AbstractDAO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejercicio_T8_2C_MVC.DAO
+{
+    public class AbstractDAO
+    {
+        public const string DB_CONNECTION_STRING
+            = "Data Source=PLX???\\SQLEXPRESS;Initial Catalog=???;Integrated Security=true";
+    }
+}
