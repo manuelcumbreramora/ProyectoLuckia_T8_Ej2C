d4c9069542dafd91961ef4d7bf59629ff2eb70d1
juan.fernandezjacob <juan.fernandezjacob@PLX300000000410>
2020-04-15 10:45:52 +0200

    Añadidas interfaces de DAO y algunas clases de Negocio.

---
+++ Negocio/Apuesta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Apuesta
+    {
+        public int Id { get; set; }
+
+        public String Usuario { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public int IdJuego { get; set; }
+    }
+}
---
+++ Negocio/Evento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Evento
+    {
+        public int Id;
+        public String Nombre { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public float Cuota { get; set; }
+
+        public String Mercado { get; set; }
+
+        public int IdJuego { get; set; }
+    }
+}
